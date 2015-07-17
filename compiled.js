faf06ba156da9c98d3c541ab8f2580e8bc2242ef
Niger Little-Poole <nigerlittlepoole@tumblr.com>
2015-07-17 00:10:41 -0400

    Histograms are a go

--- compiled.js
+++ compiled.js
@@ -92,7 +92,164 @@ Fiddle.prototype.update = function(tag){
 	func = this.figures[tag]
 	func();
 }
-Fiddle.prototype.parallel = function (tag, h, w, m){
+Fiddle.prototype.histogram = function(dimension, tag) {
+
+    if (this.data.dimensions[dimension].space==="discrete"){
+    var dataset = [];
+    var raw = {};
+    for (i =0; i < this.data.dataset.length; i++){
+	var temp = this.data.dataset[i];
+	var val = temp[dimension];
+	raw[val] = val in raw ? raw[val]  : {"name": val, "frequency":0  }  ;
+	raw[val].frequency+=1;
+    }
+    for(v in raw){
+	dataset.push(raw[v]);
+    }
+    var margin = {top: 20, right: 20, bottom: 50, left: 40},
+
+    width = 960 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+    .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+    
+    var svg = d3.select(tag).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+
+	x.domain(dataset.map(function(d) { return d.name; }));
+	y.domain([0, d3.max(dataset, function(d) { return d.frequency; })]);
+
+	svg.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis)
+	    .append("text")
+	    .attr("dy", ".71em")
+            .attr("font-size", "20px")
+            .attr("x", width/2)
+    .attr("y", 25) 
+	    .style("text-anchor", "middle")
+	    .text(dimension);
+
+	svg.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+	    .append("text")
+	    .attr("transform", "rotate(-90)")
+	    .attr("y", 6)
+	    .attr("dy", ".71em")
+	    .style("text-anchor", "end")
+	    .text("Count");
+
+	var bar = svg.selectAll(".bar")
+    .data(dataset).enter().append("g").attr("class", "bar");
+    
+       bar.append("rect")
+	    .attr("x", function(d) { return x(d.name); })
+	    .attr("width", x.rangeBand())
+	    .attr("y", function(d) { return y(d.frequency); })
+            .attr("height", function(d) { return height - y(d.frequency); });
+      bar.append("text")
+	    .attr("dy", ".75em")
+    .attr("y",  function(d) { return y(d.frequency) ; })
+            .attr("x", function(d){return x(d.name) + x.rangeBand()/2;})
+	    .attr("text-anchor", "middle")
+	    .text(function(d) { return d.frequency; });
+    }
+    else if(this.data.dimensions[dimension].space==="continuous"){
+
+	var values = [];
+	for (i =0; i < this.data.dataset.length; i++){
+	    values.push(this.data.dataset[i][dimension]);
+        }
+
+	console.log(values);
+	// A formatter for counts.
+	var formatCount = d3.format(",.0f");
+
+	var margin = {top: 10, right: 30, bottom: 30, left: 30},
+	width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
+
+	var x = d3.scale.linear()
+	.domain([Math.min.apply(Math, values), Math.max.apply(Math, values)])
+	.range([0, width]);
+
+	// Generate a histogram using twenty uniformly-spaced bins.
+	var data = d3.layout.histogram()
+	.bins(x.ticks(20))
+	(values);
+
+	var y = d3.scale.linear()
+	.domain([0, d3.max(data, function(d) { return d.y; })])
+	.range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+	.scale(x)
+	.orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	.scale(y)
+	.orient("left");
+
+	var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var bar = svg.selectAll(".bar")
+	.data(data)
+	.enter().append("g")
+	.attr("class", "bar")
+	.attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+	bar.append("rect")
+	.attr("x", 1)
+	.attr("width", x(data[0].dx) - 1)
+	.attr("height", function(d) { return height - y(d.y); });
+
+	bar.append("text")
+	.attr("dy", ".75em")
+	.attr("y", 6)
+	.attr("x", x(data[0].dx) / 2)
+	.attr("text-anchor", "middle")
+	.text(function(d) { return formatCount(d.y); });
+
+	svg.append("g")
+	.attr("class", "x axis")
+	.attr("transform", "translate(0," + height + ")")
+	.call(xAxis);
+
+	svg.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+	    .append("text")
+	    .attr("transform", "rotate(-90)")
+	    .attr("y", 6)
+	    .attr("dy", ".71em")
+	    .style("text-anchor", "end")
+	    .text("Count");
+
+    }
+};Fiddle.prototype.parallel = function (tag, h, w, m){
     var unmerged = this.data.dataset;
     var merged = {};
     var dataset = [];
@@ -279,61 +436,3 @@ function brush() {
 	});
 }
 }
-Fiddle.prototype.bar = function() {
-    dataset = this.data.dataset;
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-    var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
-
-    var y = d3.scale.linear()
-    .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10, "%");
-
-    
-    var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-
-
-	x.domain(dataset.map(function(d) { return d.letter; }));
-	y.domain([0, d3.max(dataset, function(d) { return d.frequency; })]);
-
-	svg.append("g")
-	    .attr("class", "x axis")
-	    .attr("transform", "translate(0," + height + ")")
-	    .call(xAxis);
-
-	svg.append("g")
-	    .attr("class", "y axis")
-	    .call(yAxis)
-	    .append("text")
-	    .attr("transform", "rotate(-90)")
-	    .attr("y", 6)
-	    .attr("dy", ".71em")
-	    .style("text-anchor", "end")
-	    .text("Frequency");
-
-	svg.selectAll(".bar")
-	    .data(dataset)
-	    .enter().append("rect")
-	    .attr("class", "bar")
-	    .attr("x", function(d) { return x(d.letter); })
-	    .attr("width", x.rangeBand())
-	    .attr("y", function(d) { return y(d.frequency); })
-	    .attr("height", function(d) { return height - y(d.frequency); });
-    
-
-};
--- modules/parallel.js
+++ modules/parallel.js
@@ -99,7 +99,7 @@ foreground = svg.append("svg:g")
     .on("mouseout", function(d){
             d3.select(this).transition().duration(100)
             .style({'stroke': 'steelblue' })
-            .style({'stroke-width' : '2'});
+            .style({'stroke-width' : '3'});
             return true;
         });
 // Add a group element for each dimension.
@@ -164,7 +164,7 @@ function transition(g) {
 // Returns the path for a given data point.
 function path(d) {
     var t = dimensions.map(function(p) {
-	    return [position(p), y[p](d[p])  });
+	    return [position(p), y[p](d[p]) ] });
     return line(t);
 }
 
@@ -185,61 +185,3 @@ function brush() {
 	});
 }
 }
-Fiddle.prototype.bar = function() {
-    dataset = this.data.dataset;
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-    var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
-
-    var y = d3.scale.linear()
-    .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10, "%");
-
-    
-    var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-
-
-	x.domain(dataset.map(function(d) { return d.letter; }));
-	y.domain([0, d3.max(dataset, function(d) { return d.frequency; })]);
-
-	svg.append("g")
-	    .attr("class", "x axis")
-	    .attr("transform", "translate(0," + height + ")")
-	    .call(xAxis);
-
-	svg.append("g")
-	    .attr("class", "y axis")
-	    .call(yAxis)
-	    .append("text")
-	    .attr("transform", "rotate(-90)")
-	    .attr("y", 6)
-	    .attr("dy", ".71em")
-	    .style("text-anchor", "end")
-	    .text("Frequency");
-
-	svg.selectAll(".bar")
-	    .data(dataset)
-	    .enter().append("rect")
-	    .attr("class", "bar")
-	    .attr("x", function(d) { return x(d.letter); })
-	    .attr("width", x.rangeBand())
-	    .attr("y", function(d) { return y(d.frequency); })
-	    .attr("height", function(d) { return height - y(d.frequency); });
-    
-
-};
