f691ddba3545c8cea732ccabadcd79d602da5066
Niger Little-Poole <nigerlittlepoole@tumblr.com>
2015-07-30 22:32:26 -0400

    added multivariate logistic regression and convex loss function stopping condition

--- modules/logistic_regression.js
+++ modules/logistic_regression.js
@@ -60,11 +60,14 @@ Fiddle.prototype.logisticRegression = function(dimens, criteria){
 		       features[k]["values"][v] = 0.1;
 		   }
 		}
-		if(bin==true)
-		    truth.push(dataset[i]);
-		else
-		    fal.push(dataset[i]);
+		if(i< dataset.length*.9){
+		    if(bin==true)
+			truth.push(dataset[i]);
+		    else
+			fal.push(dataset[i]);
+		}
 	    }
+
 	    var master =dataset;
 	    var testing = master.slice( Math.round(master.length*0.9) , master.length);
 
@@ -80,53 +83,89 @@ Fiddle.prototype.logisticRegression = function(dimens, criteria){
 
 
 	    beta = features;
-	    for(j =0; j<20; j++){
-		/*for(i=0; i < dataset.length; i++){
+	    /*for(j =0; j<20; j++){
+		for(i=0; i < dataset.length; i++){
 
 		features = gradientDescent(alpha, features,10, dataset[i]);
 	    }
 	    } */
 	    beta = batchGradientDescent(alpha, beta, 1, dataset);//features;
-	    }
-	    console.log(alpha);
-	    console.log(beta);
-	    var tester = generateClassifier(alpha, beta);
-	    console.log(testing[0]);
-	    console.log(tester(testing[0]));
+	    
 
+	    var tester = generateClassifier(alpha, beta);
+	    var err = {};
+	    
+	    for(i =0; i <testing.length; i++){
+		for(j =0; j<20; j++){
+		    var k = String(j*.05 + .05) + " threshold";
+  		     var res = tester(testing[i]) > j *.05 + .05 ? true: false;
+		     err[k] =   err[k]!= null ? err[k] : 0;
+		     err[k]= res === testing[i]["criteria"] ? (err[k]*i+ 0)/(i+1) : (err[k]*i + 1)/(i+1) ;
+		}
+	    }
+
+	    var result = {};
+	    result["errors"] = err;
+	    result["classifier"] = tester;
+	    return result;
     }
     else if(criteria in this.data.dimensions && this.data.dimensions[criteria].space === "discrete"){
 	var classifiers = {};
 	var vals = {};
 
+        for(i = 0; i < dataset.length; i++){
+            vals[dataset[i][criteria]] = 0;
+        }
+
+	/*
+	var index = Math.round(Math.random() * dataset.length);
+	console.log(index)
+	var test = dataset[index];
+	console.log(test); */
+        for(label in vals){
+            var crit = generateCriteria(criteria,label);
+            classifiers[label] = this.logisticRegression(dimens, crit);
+	    //console.log(label + " :" +classifiers[label]["classifier"](test));
+        }
+
+
+	return classifiers;
     }
     else{
-	console.log("stuf");
+	console.log("stuff");
 	return null;
     }
 
     function batchGradientDescent(alpha, beta, step, dataset){
+	    var result = beta;   
+	    var delta = 1;
+	    var prev = 0;
+	    while(delta > .001){
+
+
 	    var tester = generateClassifier(alpha, beta);
-
-
-	    var result = beta;
-
+	    var error = 0;
+ 
 	    for(i =0; i< dataset.length; i++){
 		var test = dataset[i];
-	    var prob = tester(test);
-	    var err = test["criteria"] === true ? 1 - prob : 0 - prob; 
-	    for(feature in beta){
-		var map = beta[feature]["map"];
-		var val = map(test[feature]);
+		var prob = tester(test);
+		var err = test["criteria"] === true ? 1 - prob : 0 - prob;
+		error += Math.abs(err);
+	        for(feature in beta){
+		    var map = beta[feature]["map"];
+		    var val = map(test[feature]);
 		
-		var a = beta[feature]["values"][val];
-		partial = -1*err *( a *Math.exp(a))/ Math.pow(( Math.exp(a)+ 1)  ,2);
-		var gradient = partial * step;
+		    var a = beta[feature]["values"][val];
+		    partial = -1*err *( a *Math.exp(a))/ Math.pow(( Math.exp(a)+ 1)  ,2);
+		    var gradient = partial * step;
 
-		result[feature]["values"][val] = result[feature]["values"][val] - gradient;
+        	    result[feature]["values"][val] = result[feature]["values"][val] - gradient;
+		}
 	    }
-	    }
-	    return result;
+	    delta = prev === 0 ? delta : 1 - (error /prev);
+	    prev = error;
+	}
+	return result;
 		
 
 
